9d5914b134a5430779a5e54c10083d2a2cf03d3c
Derick Castillo <derick.castillo@daveramsey.com>
2025-04-23 14:21:18 -0500

    Setup blogpost repository

---
+++ src/main/java/com/blog/blogsite/repository/BlogpostRepository.java
@@ -0,0 +1,9 @@
+package com.blog.blogsite.repository;
+
+import com.blog.blogsite.entity.Blogpost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogpostRepository extends JpaRepository<Blogpost, Long> {
+}
